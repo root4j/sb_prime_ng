a9495b86398a29920ec923f0c64c07ee4dcab1c2
Ruthford Jay <ruthford.jay@cannoncol.com>
2022-03-03 18:06:54 -0500

    Creacion del proyecto frontend utilizando Angular + PrimeNG

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AppMainComponent } from './app.main.component';
+import { CrudComponent } from './components/crud/crud.component';
+import { ErrorComponent } from './components/error/error.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { AccessComponent } from './components/access/access.component';
+@NgModule({
+    imports: [
+        RouterModule.forRoot([
+            {
+                path: '', component: AppMainComponent,
+                children: [
+                    {path: '', component: DashboardComponent},
+                    {path: 'pages/crud', component: CrudComponent},
+                ],
+            },
+            {path:'pages/error', component: ErrorComponent},
+            {path:'pages/notfound', component: NotfoundComponent},
+            {path:'pages/access', component: AccessComponent},
+            {path: '**', redirectTo: 'pages/notfound'},
+        ], {scrollPositionRestoration: 'enabled', anchorScrolling:'enabled'})
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
---
+++ frontend/src/app/app.menu.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { AppMainComponent } from './app.main.component';
+
+@Component({
+    selector: 'app-menu',
+    template: `
+        <div class="layout-menu-container">
+            <ul class="layout-menu" role="menu" (keydown)="onKeydown($event)">
+                <li app-menu class="layout-menuitem-category" *ngFor="let item of model; let i = index;" [item]="item" [index]="i" [root]="true" role="none">
+                    <div class="layout-menuitem-root-text" [attr.aria-label]="item.label">{{item.label}}</div>
+                    <ul role="menu">
+                        <li app-menuitem *ngFor="let child of item.items" [item]="child" [index]="i" role="none"></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    `
+})
+export class AppMenuComponent implements OnInit {
+
+    model: any[];
+
+    constructor(public appMain: AppMainComponent) { }
+
+    ngOnInit() {
+        this.model = [
+            {
+                label: 'Menu',
+                items: [
+                    {label: 'Item Normal', icon: 'pi pi-fw pi-bookmark', routerLink: ['/']},
+                    {
+                        label: 'Submenu 1', icon: 'pi pi-fw pi-bookmark',
+                        items: [
+                            {
+                                label: 'Submenu 1.1', icon: 'pi pi-fw pi-bookmark',
+                                items: [
+                                    {label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark', routerLink: ['/']},
+                                    {label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark'},
+                                    {label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark'},
+                                ]
+                            },
+                            {
+                                label: 'Submenu 1.2', icon: 'pi pi-fw pi-bookmark',
+                                items: [
+                                    {label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark'}
+                                ]
+                            },
+                        ]
+                    },
+                ]
+            }
+        ];
+    }
+
+    onKeydown(event: KeyboardEvent) {
+        const nodeElement = (<HTMLDivElement> event.target);
+        if (event.code === 'Enter' || event.code === 'Space') {
+            nodeElement.click();
+            event.preventDefault();
+        }
+    }
+}
